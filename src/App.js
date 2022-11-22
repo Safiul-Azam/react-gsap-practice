4548238d18b8e1960945694d2af1e0ca8404b865
Safiul-Azam <safiulazamriad@gmail.com>
2022-11-22 12:03:04 +0600

    start react gsap practice project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { TweenMax, Power3 } from "gsap";
+import { useEffect, useRef } from "react";
 function App() {
+  let logoItem = useRef(null);
+  let textItem = useRef(null);
+  useEffect(() => {
+    console.log(logoItem);
+    TweenMax.to(logoItem, 0.8, { opacity: 1, y: -20, ease: Power3.easeOut });
+    TweenMax.to(textItem, 0.8, { opacity: 1, y: -20, ease: Power3.easeOut,delay:.5 });
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <img
+          ref={(el) => {
+            logoItem = el;
+          }}
+          src={logo}
+          className="App-logo"
+          alt="logo"
+        />
+        <p
+          ref={(el) => {
+            textItem = el;
+          }}
+        >
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
