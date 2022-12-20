648d5a08fc80d7af0577aa6c2883879c74b409ec
Safiul-Azam <safiulazamriad@gmail.com>
2022-12-20 11:29:20 +0600

    added gsap

--- src/component/Box.jsx
+++ src/component/Box.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import gsap from "gsap";
+import { useEffect } from "react";
 
 const Box = () => {
-    return (
-        <div className='space-y-10'>
-            <div className='w-20 h-20 bg-green-900'></div>
-            <div className='w-20 h-20 bg-red-900'></div>
-            <div className='w-20 h-20 bg-yellow-900'></div>
-        </div>
-    );
+  useEffect(() => {
+    gsap.to(".a", {
+      x: 400,
+      rotation: 360,
+      duration: 4,
+      delay:1
+    });
+  }, []);
+  return (
+    <div className="space-y-20">
+      <div className="w-20 h-20 bg-green-900 a"></div>
+      <div className="w-20 h-20 bg-red-900 b"></div>
+      <div className="w-20 h-20 bg-yellow-900 c"></div>
+    </div>
+  );
 };
 
 export default Box;
