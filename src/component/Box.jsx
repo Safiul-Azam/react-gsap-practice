aa90dec26d27f2160b9a65772448f0d1feff6424
Safiul-Azam <safiulazamriad@gmail.com>
2022-12-20 09:29:20 +0600

    added space in box

--- src/component/Box.jsx
+++ src/component/Box.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Box = () => {
     return (
-        <div>
+        <div className='space-y-10'>
             <div className='w-20 h-20 bg-green-900'></div>
             <div className='w-20 h-20 bg-red-900'></div>
             <div className='w-20 h-20 bg-yellow-900'></div>
