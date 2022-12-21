439fe0a3340cde740e811f9d788ec42e8fec4364
Safiul-Azam <safiulazamriad@gmail.com>
2022-12-21 08:12:20 +0600

    change some

--- src/component/Box.jsx
+++ src/component/Box.jsx
@@ -1,17 +1,23 @@
 import gsap from "gsap";
 import { useEffect } from "react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
 const Box = () => {
   useEffect(() => {
-    gsap.to(".a", {
+    gsap.to(".c", {
+      scrollTrigger:{
+        trigger:".c",
+        toggleActions:"play none none none"
+      },
       x: 400,
       rotation: 360,
       duration: 4,
-      delay:1
+      delay: 1,
     });
   }, []);
   return (
-    <div className="space-y-20">
+    <div className="overflow-y-scroll h-40 border">
       <div className="w-20 h-20 bg-green-900 a"></div>
       <div className="w-20 h-20 bg-red-900 b"></div>
       <div className="w-20 h-20 bg-yellow-900 c"></div>
